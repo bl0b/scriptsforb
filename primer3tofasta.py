cbb98ad5f1b3c7e27191b2b84cd22be33993ed82
Damien Leroux <damien.leroux@gmail.com>
2012-03-19 00:00:53 +0100

    Reformatted script. Added support for html format.

--- primer3tofasta.py
+++ primer3tofasta.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
 from splitter import splitter
-import sys, re
+import sys
+import re
 from blast_parser import *
+from bs4 import BeautifulSoup as bs
 
 quit_skipping = "start  len      tm     gc%   any    3' seq"
 resume_skipping = "Statistics"
 
-fint_ = '[0-9]+'
-fint = '('+fint_+')'
-ffloat = '('+fint_+r'\.'+fint_+')'
+fint_ = '[0-9] + '
+fint = '(' + fint_ + ')'
+ffloat = '(' + fint_ + r'\.' + fint_ + ')'
 fspace = ' +'
-LR_COMMON = fspace.join([fint, fint, ffloat, ffloat, ffloat, ffloat, '([tcagACGT]+)'])
-LEFT_RE = re.compile(fint+' +LEFT PRIMER +'+LR_COMMON)
-RIGHT_RE = re.compile('RIGHT PRIMER +'+LR_COMMON)
-PRODUCT_SIZE_RE = re.compile('PRODUCT SIZE: '+fint+', PAIR ANY COMPL: '+ffloat+", PAIR 3' COMPL: "+ffloat)
+LR_COMMON = fspace.join([fint, fint, ffloat, ffloat, ffloat, ffloat,
+                         '([tcagACGT] + )'])
+LEFT_RE = re.compile(fint + ' +LEFT PRIMER +' + LR_COMMON)
+RIGHT_RE = re.compile('RIGHT PRIMER +' + LR_COMMON)
+PRODUCT_SIZE_RE = re.compile('PRODUCT SIZE: ' + fint + ', PAIR ANY COMPL: ' +
+                             ffloat + ", PAIR 3' COMPL: " + ffloat)
 
-lr_fields = ( 'start', 'len', 'tm', 'gc%', 'any', "3'", 'seq' )
-lr_types = ( int, int, float, float, float, float, str)
+lr_fields = ('start', 'len', 'tm', 'gc%', 'any', "3'", 'seq')
+lr_types = (int, int, float, float, float, float, str)
+
 
 def match2type(fields):
     return [lr_types[i](fields[i]) for i in xrange(len(fields))]
+
 
 class primer3(object):
     def __init__(self):
@@ -30,6 +36,7 @@ class primer3(object):
         self.product_size = None
         self.pair_any_compl = None
         self.pair_3_compl = None
+
     def add_line(self, l):
         try:
             if self.left is None:
@@ -43,27 +50,36 @@ class primer3(object):
                 self.right = dict(zip(lr_fields, match2type(fields)))
             elif self.product_size is None:
                 #print "misc>", l
-                self.product_size, self.pair_any_compl, self.pair_3_compl = PRODUCT_SIZE_RE.match(l).groups()
+                self.product_size, self.pair_any_compl, self.pair_3_compl = (
+                    PRODUCT_SIZE_RE.match(l).groups())
             return self.product_size is not None
         except Exception, e:
             print "Couldn't parse file around line :"
             print l
             sys.exit(0)
+
     def __str__(self):
-        return '<primer3 #%i left=%s right=%s>'%(self.num, self.left['seq'], self.right['seq'])
+        return '<primer3 #%i left = %s right = %s>' % (self.num,
+                                                       self.left['seq'],
+                                                       self.right['seq'])
+
     def __repr__(self):
-        return '<primer3 #%i left=%s right=%s>'%(self.num, self.left['seq'], self.right['seq'])
+        return '<primer3 #%i left = %s right = %s>' % (self.num,
+                                                       self.left['seq'],
+                                                       self.right['seq'])
 
 
 def primer3_parse(f):
-    ret = [ None ]
+    ret = [None]
     if type(f) is str:
         f = open(f)
     skipping = True
     get_header = False
-    header = [ 'label' ]
+    header = ['label']
     buf = f.read()
-    sep=''
+    if 'DOCTYPE html' in buf:
+        buf = bs(buf)('pre')[0].text
+    sep = ''
     if '\r' in buf:
         sep = '\r'
     if '\n' in buf:
@@ -74,10 +90,10 @@ def primer3_parse(f):
         if not l:
             continue
         if skipping:
-            if l==quit_skipping:
+            if l == quit_skipping:
                 skipping = False
             continue
-        elif l==resume_skipping:
+        elif l == resume_skipping:
             skipping = True
             continue
         if ret[-1] is None:
@@ -89,16 +105,18 @@ def primer3_parse(f):
     return ret
 
 
-
-
 def primer_to_fasta(p, lr, pfields):
-    return '>%i %s %i | product size: %s, pair any compl.: %s, pair 3\' compl.: %s\n%s\n'%(p.num, lr, pfields['start'], p.product_size, p.pair_any_compl, p.pair_3_compl, pfields['seq'])
+    return ('>%i %s %i | product size: %s, pair any compl.: %s,'
+            + ' pair 3\' compl.: %s\n%s\n') % (
+                p.num, lr, pfields['start'], p.product_size, p.pair_any_compl,
+                p.pair_3_compl, pfields['seq'])
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     for inputfile in sys.argv[1:]:
         inputfile = sys.argv[1]
         test = primer3_parse(inputfile)
-        print >> open(inputfile+'.fasta', 'w'), '\n'.join(map(lambda p: primer_to_fasta(p, 'left', p.left)+primer_to_fasta(p, 'right', p.right), test))
-
+        output = map(lambda p: primer_to_fasta(p, 'left', p.left)
+                               + primer_to_fasta(p, 'right', p.right),
+                     test)
+        print >> open(inputfile + '.fasta', 'w'), '\n'.join(output)
