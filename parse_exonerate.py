9f1d86a2a2f8d20c63cf38b4b22d71cbafb11c7b
Damien Leroux <damien.leroux@gmail.com>
2012-08-06 23:15:28 +0200

    Now parsing the alignment output (alpha).

--- parse_exonerate.py
+++ parse_exonerate.py
@@ -1,7 +1,9 @@
 from parse_gff2 import parse_gff2
+import re
 
 START_GFF = "# --- START OF GFF DUMP ---"
 END_GFF = "# --- END OF GFF DUMP ---"
+C4_ALIGNMENT = 'C4 Alignment:'
 
 
 class exonerate_file(file):
@@ -19,3 +21,162 @@ class exonerate_file(file):
 
 def parse_exonerate(filename):
     return parse_gff2(exonerate_file(filename))
+
+
+c4 = ("         Query: gi|327271245|ref|XP_003220398.1| PREDICTED:"
+      + " uncharacterized protein C6orf130-like isoform 1 [Anolis"
+      + " carolinensis] >gi|327271247|ref|XP_003220399.1| PREDICTED:"
+      + " uncharacterized protein C6orf130-like isoform 2 [Anolis"
+      + """ carolinensis]
+        Target: Gi_4536_gatcag5825_mbl1599_2MIRA_mapping Average"""
+      + """ coverage: 73,89:[revcomp]
+         Model: protein2dna:bestfit
+     Raw score: 328
+   Query range: 0 -> 150
+  Target range: 1849 -> 1396
+
+    1 : MetAlaSerIleAsnProGluLysGluGluArgIleValTyrArgGlnGlyAspLeuPheTh :   21
+        :!::!!|||! !!.. !!   :!!:!!   ! !.!!..! ! ||||||||||||||||||||
+        LeuSerSerThrThrSerIleGluLysPheThrPheThrGluArgGlnGlyAspLeuPheTh
+ 1849 : TTATCATCGACAACATCAATTGAAAAATTTACTTTTACAGAACGACAAGGAGACTTATTCAC : 1789
+
+   22 : r<->CysProGluThrAspAlaLeuAlaHisCysIleSerGluAspCysHisMetSerAlaG :   41
+        |   ...|||  !|||||||||||||||||||||:!!|||:!!|||  !...|||.!!  !|
+        rAspAlaProProThrAspAlaLeuAlaHisCysValSerGlnAspLeuArgMetGlyLysG
+ 1788 : TGATGCACCACCAACTGACGCGTTAGCTCATTGCGTATCTCAAGATCTTAGAATGGGTAAAG : 1726
+
+   42 : lyIleAlaAlaValPheLysLysLysPheGlyGlyIleGlnGluLeuLeuAsnGlnGlnLys :   61
+        ||||||||  !:!!||||||||||||!:!! !|||:!!.!.||||||  !  !||||||""" + "   "
+      + """
+        lyIleAlaTyrIlePheLysLysLysTyrGluGlyLeuAsnGluLeuLysAlaGlnGlnCys
+ 1725 : GTATCGCATATATTTTCAAGAAAAAATATGAAGGATTAAATGAATTGAAAGCTCAACAATGT : 1666
+
+   62 : LysThrGlyAspValAlaValLeuLysArgAspAsnArgTyrValTyrTyrLeuIleThrLy :   82
+        |||..!|||.!.||||||  !|||:!!|||||||||||||||:!!!:!||||||||||||||
+        LysValGlyGlnValAlaTyrLeuGlnArgAspAsnArgTyrIlePheTyrLeuIleThrLy
+ 1665 : AAAGTCGGTCAAGTTGCTTATCTTCAACGTGATAATCGTTATATTTTTTATTTAATTACTAA : 1603
+
+   83 : sAsnLysTyrPheHisLysProThrTyrAspAsnLeuGlnLysSerLeuAspAlaMetLysL :  103
+        | !! !   !!:! !!||||||  !  ! ! .!.!!!:!!! !||||||!  ! !:!!!:!""" + " "
+      + """
+        sTyrTyrValTyrAspLysProAspArgLysGluPheGluThrSerLeuValGluLeuArgA
+ 1602 : ATATTATGTTTACGATAAACCTGATCGTAAAGAATTTGAAACAAGTTTGGTGGAATTGAGAA : 1540
+
+  104 : euHisCysValGluAsnGlyValThrArgIleSerMetProLysIleGlyCysGlyLeuAsp :  123
+         !   |||! !:!!  !||||||! ! !!:!!|||:!:|||::!||||||  !|||||||||
+        rgLeuCysGluGlnLeuGlyValArgGlyLeuSerValProArgIleGlyThrGlyLeuAsp
+ 1539 : GGTTGTGTGAACAACTTGGTGTTAGAGGTTTAAGTGTTCCTCGGATTGGAACTGGATTGGAT : 1480
+
+  124 : ArgLeuAspTrpGluArgValSerThrMetLeuGluGluValPheGluGlyThrAspValTy :  144
+         !!|||..!! !...  !|||!.!!:!   :!!.!.|||!.!|||||||||!:!:!!:!! !
+        GlyLeuSerLeuSerTyrValLysSerAlaIleAsnGluAlaPheGluGlySerAsnIleLy
+ 1479 : GGGTTATCTTTGAGTTATGTTAAAAGTGCTATTAACGAAGCCTTTGAAGGGAGTAATATCAA : 1417
+
+  145 : rIleThrValTyrIleLeu :  150
+         :!!|||:!!!:!  !|||
+        sValThrMetPheTyrLeu
+ 1416 : AGTTACTATGTTTTATTTG : 1397
+""")
+
+
+class Query(object):
+
+    def __init__(self, f):
+
+        def nextline():
+            l = ''
+            while not l:
+                l = f.next()
+            #print l
+            return l
+
+        self.raw_query = nextline().strip()
+        self.raw_target = nextline().strip()
+        self.raw_model = nextline().strip()
+        self.raw_raw_score = nextline().strip()
+        self.raw_query_range = nextline().strip()
+        self.raw_target_range = nextline().strip()
+        self.target_range = self.raw_target_range.split(':')[1].split(' -> ')
+        self.target_range = map(int, self.target_range)
+
+        self.query_prot = ""
+        self.target_prot = ""
+        self.target_seq = ""
+        self.alignment = ""
+        self.vulgar = ""
+        self.gff = ""
+
+        self.state = [self._rd_blank,
+                      self._rd_q,
+                      self._rd_al,
+                      self._rd_tgt,
+                      self._rd_dna]
+        i = 0
+
+        while self.state[i](nextline()):
+            i = (i + 1) % len(self.state)
+            #print i
+
+    def _rd_blank(self, l):
+        return True
+
+    def _rd_q(self, l):
+        if l.startswith('vulgar'):
+            return False
+        #print "read query", l.strip()
+        self.query_prot += l.split(' : ')[1]
+        return True
+
+    def _rd_al(self, l):
+        #print "read alignment", l.strip()
+        self.alignment += l[8:]
+        return True
+
+    def _rd_tgt(self, l):
+        #print "read target", l.strip()
+        self.target_prot += l[8:]
+        return True
+
+    def _rd_dna(self, l):
+        #print "read target DNA", l.strip()
+        s = l.split(' : ')
+        self.target_seq += s[1]
+        #print l
+        #print "0123456789" * 8
+        #print int(s[2].strip()), self.target_range[1]
+        a = int(s[2].strip())
+        b = self.target_range[1]
+        return a != b and a != b + 1
+        #return int(s[2].strip()) != self.target_range[1] + 1
+
+
+class Exonerate(object):
+
+    def __init__(self, filename):
+        f = open(filename)
+        self.commandline = re.match(r'Command line: \[([^]]*)',
+                                    f.readline()).group(1)
+        self.hostname = re.match(r'Hostname: \[([^]]*)',
+                                 f.readline()).group(1)
+
+        self.queries = []
+
+        while True:
+            try:
+                l = f.next()
+            except StopIteration, si:
+                break
+            if not l:
+                continue
+            if l.startswith(C4_ALIGNMENT):
+                f.next()  # skip ------
+                self.queries.append(Query(f))
+            elif l.startswith('vulgar:'):
+                self.queries[-1].vulgar = l[7:].strip()
+            elif l.startswith(START_GFF):
+                gff = []
+                l = f.next()
+                while not l.startswith(END_GFF):
+                    gff.append(l)
+                    l = f.next()
+                self.queries[-1].gff = parse_gff2(iter(gff))
