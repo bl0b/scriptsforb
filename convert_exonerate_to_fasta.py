1e34830c78bee5878de4207b62a472fd0289080a
Damien Leroux <damien.leroux@gmail.com>
2012-11-08 15:49:45 +0100

    Modified stripper's behaviour.

--- convert_exonerate_to_fasta.py
+++ convert_exonerate_to_fasta.py
@@ -15,18 +15,22 @@ if '-h' in args or '--help' in args:
 if args[0] == '-s':
     args = args[1:]
     pred_a = lambda x: x != '#'
+    ts_remove = set(['.', 'a', 'c', 't', 'g', '{', '}'])
+    pred_ts = lambda x: x not in ts_remove
+    suffix = '_STRIPPED'
 else:
     pred_a = lambda x: True
+    suffix = ''
 
 
 for filename in args:
     e = Exonerate(filename)
-    out = open(filename + '.fas', 'w')
+    out = open(filename + suffix + '.fas', 'w')
     for q in e.queries:
         tseq = ''.join(ts
                        for qp, tp, ts, a
-                        in q.iterate(pred_a=pred_a))
+                        in q.iterate(pred_a=pred_a, pred_ts=pred_ts))
         s = Sequence(q.Query.split(' ')[0] + '_' + q.Target
-                     + ('#' in q.alignment and '_STRIPPED' or ''),
+                     + ('#' in q.alignment and suffix or ''),
                      tseq.replace('-', 'N'))
         print >> out, s
